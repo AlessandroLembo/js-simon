224d69440a9e34b920c035f7d6eed9b5efda5840
AlessandroLembo <alessandrolembo2@gmail.com>
2022-12-20 16:29:16 +0100

    recupero elementi dalla pagina

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 console.log('JS OK');
+
+// Prendo gli elementi dalla pagina.
+const daysElement = document.getElementById('days');
+const hoursElement = document.getElementById('hours');
+const minutesElement = document.getElementById('minutes');
+const secondsElement = document.getElementById('seconds');
+// console.log(daysElement, hoursElement, minutesElement, secondsElement);
